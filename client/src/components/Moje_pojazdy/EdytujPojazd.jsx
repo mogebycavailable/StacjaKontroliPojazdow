46702b46e4a266443cf53e0d92cc1e0a43b8ee33
Tomasz Brzozowski <brzoztomasz@gmail.com>
2024-11-03 19:02:08 +0100

    Rozpoczecie tworzenia obslugi edycji dodanych pojazdow

--- client/src/components/Moje_pojazdy/EdytujPojazd.jsx
+++ client/src/components/Moje_pojazdy/EdytujPojazd.jsx
@@ -5,6 +5,12 @@ import './MojePojazdy.css'
 import vehicle_icon from '../css/img/vehicle.png';
 
 const EdytujPojazd = () => {
+    const vehicleArray = [
+        {id: 1, marka: 'Mitsubishi', model: 'Carisma', rokProdukcji: 2003, nrRejestracyjny: 'ABC 12345', nrVin: 'JHMFA16546S014841', nastepneBadanie: '2025-01-10'},
+        {id: 2, marka: 'Citroen', model: 'Xsara', rokProdukcji: 2003, nrRejestracyjny: 'DEF 67890', nrVin: '1HGCM82633A004352', nastepneBadanie: '2024-06-02'},
+        {id: 3, marka: 'Toyota', model: 'Corolla', rokProdukcji: 2005, nrRejestracyjny: 'GHI 11223', nrVin: 'FA36BVFSD33FSDDF4', nastepneBadanie: '2025-10-07'},
+    ]
+
     return(
         <div>
             
--- client/src/components/Moje_pojazdy/MojePojazdy.jsx
+++ client/src/components/Moje_pojazdy/MojePojazdy.jsx
@@ -5,43 +5,45 @@ import './MojePojazdy.css'
 import vehicle_icon from '../css/img/vehicle.png';
 
 const MojePojazdy = () => {
+    const vehicleArray = [
+        {id: 1, marka: 'Mitsubishi', model: 'Carisma', rokProdukcji: 2003, nrRejestracyjny: 'ABC 12345', nrVin: 'JHMFA16546S014841', nastepneBadanie: '2025-01-10'},
+        {id: 2, marka: 'Citroen', model: 'Xsara', rokProdukcji: 2003, nrRejestracyjny: 'DEF 67890', nrVin: '1HGCM82633A004352', nastepneBadanie: '2024-06-02'},
+        {id: 3, marka: 'Toyota', model: 'Corolla', rokProdukcji: 2005, nrRejestracyjny: 'GHI 11223', nrVin: 'FA36BVFSD33FSDDF4', nastepneBadanie: '2025-10-07'},
+    ]
+
+    const today = new Date().toISOString().split('T')[0]
+
     return(
         <div className='body-div'>
             <h2>Moje pojazdy</h2>
             <div className='moje_pojazdy-main-div'>
-                <button id="add"><Link to="/moje_pojazdy/dodaj_pojazd" className="btn">Dodaj</Link></button>
-                <div className="vehicle">
-                    <div className="photo">
-                        <img src={vehicle_icon}/>
+                <Link to="/moje_pojazdy/dodaj_pojazd">
+                    <button id="add">Dodaj</button>
+                </Link>
+                {vehicleArray.map((vehicle) => {
+                    const dateStyle = { color: vehicle.nastepneBadanie < today ? 'red' : 'green' };
+                    const isExpired = vehicle.nastepneBadanie < today;
+                    const statusText = isExpired ? "(NIEWAŻNE)" : "(WAŻNE)";
+                    return (
+                    <div className="vehicle">
+                        <div className="photo">
+                            <img src={vehicle_icon}/>
+                        </div>
+                        <div key={vehicle.id} className="data">
+                            <h4>Marka: {vehicle.marka}</h4>
+                            <h4>Model: {vehicle.model}</h4>
+                            <h4>Rok produkcji: {vehicle.rokProdukcji}</h4>
+                            <h4>Nr rejestracyjny: {vehicle.nrRejestracyjny}</h4>
+                            <h4>Nr VIN: {vehicle.nrVin}</h4>
+                            <h4>Badania techniczne:   <span style={dateStyle}>{vehicle.nastepneBadanie} {statusText}</span></h4>
+                            <Link to={`/moje_pojazdy/edytuj_pojazd/${vehicle.id}`}>
+                                <button id="edit">Edytuj dane</button>
+                            </Link>
+                            <button id="delete">Usuń pojazd</button>
+                            <button id="order">Umów się na przegląd</button>
+                        </div>
                     </div>
-                    <div className="data">
-                        <h4>Marka: Mitsubishi</h4>
-                        <h4>Model: Carisma</h4>
-                        <h4>Rok produkcji: 2003</h4>
-                        <h4>Numer rejestracyjny: ABC 12345</h4>
-                        <h4>VIN: JHMFA16546S014841</h4>
-                        <h4>Badanie techniczne: <span className="wazne">ważne do 10.01.2025</span></h4>
-                        <button id="edit">Edytuj dane</button>
-                        <button id="delete">Usuń pojazd</button>
-                        <button id="order">Umów się na przegląd</button>
-                    </div>
-                </div>			
-                <div className="vehicle">
-                    <div className="photo">
-                        <img src={vehicle_icon}/>
-                    </div>
-                    <div className="data">
-                        <h4>Marka: Citroen</h4>
-                        <h4>Model: Xsara</h4>
-                        <h4>Rok produkcji: 2003</h4>
-                        <h4>Numer rejestracyjny: DEF 67890</h4>
-                        <h4>VIN: 1HGCM82633A004352</h4>
-                        <h4>Badanie techniczne: <span className="niewazne">nie ważne od 02.06.2024</span></h4>
-                        <button id="edit">Edytuj dane</button>
-                        <button id="delete">Usuń pojazd</button>
-                        <button id="order">Umów się na przegląd</button>
-                    </div>
-                </div>
+                );})}
             </div>
 	    </div>
     );
