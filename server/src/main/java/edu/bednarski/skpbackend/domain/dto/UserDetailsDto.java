7efdb783b3fbee82f8e1fcedd15f470eb54104fc
Piotrek Bednarski <piotrbednarski24@gmail.com>
2024-12-15 21:14:51 +0100

    Poprawiono blad z Lombok

--- server/src/main/java/edu/bednarski/skpbackend/domain/dto/UserDetailsDto.java
+++ server/src/main/java/edu/bednarski/skpbackend/domain/dto/UserDetailsDto.java
@@ -2,10 +2,8 @@ package edu.bednarski.skpbackend.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -19,4 +17,77 @@ public class UserDetailsDto {
 
     private String phone;
 
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof UserDetailsDto)) return false;
+        final UserDetailsDto other = (UserDetailsDto) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$surname = this.getSurname();
+        final Object other$surname = other.getSurname();
+        if (this$surname == null ? other$surname != null : !this$surname.equals(other$surname)) return false;
+        final Object this$email = this.getEmail();
+        final Object other$email = other.getEmail();
+        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
+        final Object this$phone = this.getPhone();
+        final Object other$phone = other.getPhone();
+        if (this$phone == null ? other$phone != null : !this$phone.equals(other$phone)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof UserDetailsDto;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $surname = this.getSurname();
+        result = result * PRIME + ($surname == null ? 43 : $surname.hashCode());
+        final Object $email = this.getEmail();
+        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
+        final Object $phone = this.getPhone();
+        result = result * PRIME + ($phone == null ? 43 : $phone.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "UserDetailsDto(name=" + this.getName() + ", surname=" + this.getSurname() + ", email=" + this.getEmail() + ", phone=" + this.getPhone() + ")";
+    }
 }
