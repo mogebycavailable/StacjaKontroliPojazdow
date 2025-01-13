521962104cfe02c2f767476dc8b3836c5b26f1b8
Piotrek Bednarski <piotrbednarski24@gmail.com>
2025-01-13 20:34:15 +0100

    Zaimplementowano pobieranie wszystkich przegladow uzytkownika.

---
+++ server/src/main/java/edu/bednarski/skpbackend/controllers/UserInspectionsController.java
@@ -0,0 +1,27 @@
+package edu.bednarski.skpbackend.controllers;
+
+import edu.bednarski.skpbackend.domain.dto.InspectionDetailsDto;
+import edu.bednarski.skpbackend.services.InspectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/user")
+@RequiredArgsConstructor
+public class UserInspectionsController {
+
+    private final InspectionService service;
+
+    @GetMapping(path = "/inspections")
+    public ResponseEntity<?> findAllUsersInspections() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<InspectionDetailsDto> allUsersInspections = service.findAllUsersInspections(email);
+        return new ResponseEntity<>(allUsersInspections, HttpStatus.OK);
+    }
+}
