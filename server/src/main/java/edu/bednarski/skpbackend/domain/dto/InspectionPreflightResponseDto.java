c19ba57f0ef6af345dae8be4028cc88304a4e3b1
Piotrek Bednarski <piotrbednarski24@gmail.com>
2025-01-14 19:01:41 +0100

    Zredukowano rozmiar odpowiedzi z wyszukiwania wolnych godzin - usunieto duplikaty przesylanych informacji

--- server/src/main/java/edu/bednarski/skpbackend/domain/dto/InspectionPreflightResponseDto.java
+++ server/src/main/java/edu/bednarski/skpbackend/domain/dto/InspectionPreflightResponseDto.java
@@ -1,20 +1,2 @@
-package edu.bednarski.skpbackend.domain.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Builder
-public class InspectionPreflightResponseDto {
-
-    private String time;
-
-    private StandDto stand;
-
-    private VehicleDto vehicle;
-
+package edu.bednarski.skpbackend.domain.dto;public class InspectionPreflightResponseDto {
 }
